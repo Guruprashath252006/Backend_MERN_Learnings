d64a2938ad9ff25e7290493cac935a590cadc641
Guruprashath <guruprashath25@gmail.com>
2026-02-24 21:06:05 +0530

    Update for_loops.js

--- for_loops.js
+++ for_loops.js
@@ -3,4 +3,20 @@ let n=cars.length
 
 for (let i=0;i<n;i++){
     console.log(cars[i])
+
 }
+
+//2
+
+function isprime(num){
+    if (num <= 1){
+        return false;
+    }
+    for (let i = 2; i * i <= num; i++){
+        if(num % i ===0){
+        return false;
+        }
+    }
+    return true;
+}
+    
