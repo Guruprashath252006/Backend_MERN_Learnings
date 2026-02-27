3c83cc43c96b435b771209edbdfbcc80f7e391a6
Guruprashath <guruprashath25@gmail.com>
2026-02-27 15:25:21 +0530

    Update for_loops.js

--- for_loops.js
+++ for_loops.js
@@ -30,4 +30,13 @@ for (let i = 3; i <= 29; i++) {
     }
 }
 
+//4
+let n = 5;
+for (let i = 1; i <= n; i++) { 
+  let row = "";
+  for (let j = 1; j <= i; j++) { 
+    row += "* ";
+  }
+  console.log(row);
+}
 
