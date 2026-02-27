b859f1010fa9a761d68debad674751427ee2fd46
Guruprashath <guruprashath25@gmail.com>
2026-02-27 22:15:50 +0530

    Add additional for loop example for pattern printing

--- for_loops.js
+++ for_loops.js
@@ -40,3 +40,13 @@ for (let i = 1; i <= n; i++) {
   console.log(row);
 }
 
+
+//5
+for (let i = 1; i <= 4; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+        row += "* ";
+    }
+    console.log(row);
+}
+
