f31f5fe11e95668466487c0878cbd6a12976294d
Guruprashath <guruprashath25@gmail.com>
2026-03-01 16:41:50 +0530

    Update for_loops.js

--- for_loops.js
+++ for_loops.js
@@ -54,3 +54,77 @@ for (let i = 1; i <= 4; i++) {
 }
 
 
+// task 8
+
+// 1. Square
+const size = 5;
+for (let i = 0; i < size; i++) {
+  console.log("* ".repeat(size));
+}
+
+// 2. Rectangle
+const rowsRect = 3, colsRect = 10;
+for (let i = 0; i < rowsRect; i++) {
+  console.log("* ".repeat(colsRect));
+}
+
+// 3. Right triangle
+const n = 5;
+for (let i = 1; i <= n; i++) {
+  console.log("* ".repeat(i));
+}
+
+// 4. Left triangle (Right-aligned)
+const nLT = 5;
+for (let i = 1; i <= nLT; i++) {
+  console.log("  ".repeat(nLT - i) + "* ".repeat(i));
+}
+
+// 5. Inverted triangle
+const nIT = 5;
+for (let i = nIT; i >= 1; i--) {
+  console.log("* ".repeat(i));
+}
+
+// 6. Pyramid
+const nP = 5;
+for (let i = 1; i <= nP; i++) {
+  console.log(" ".repeat(nP - i) + "* ".repeat(i));
+}
+
+// 7. Inverted pyramid
+const nIP = 5;
+for (let i = nIP; i >= 1; i--) {
+  console.log(" ".repeat(nIP - i) + "* ".repeat(i));
+}
+
+// 8. Diamond
+const nD = 5;
+for (let i = 1; i <= nD; i++) {
+  console.log(" ".repeat(nD - i) + "* ".repeat(i));
+}
+for (let i = nD - 1; i >= 1; i--) {
+  console.log(" ".repeat(nD - i) + "* ".repeat(i));
+}
+
+// 9. Hollow square
+const sHS = 5;
+for (let i = 0; i < sHS; i++) {
+  if (i === 0 || i === sHS - 1) {
+    console.log("* ".repeat(sHS));
+  } else {
+    console.log("* " + "  ".repeat(sHS - 2) + "*");
+  }
+}
+
+// 10. Hollow triangle
+const nHT = 5;
+for (let i = 1; i <= nHT; i++) {
+  if (i === 1 || i === nHT) {
+    console.log("* ".repeat(i));
+  } else {
+    console.log("* " + "  ".repeat(i - 2) + "*");
+  }
+}
+
+
